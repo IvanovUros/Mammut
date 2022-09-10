da92bdf3a679b77001e473fa7665d85fd8d6296d
IvanovUros <urivanov03@gmail.com>
2022-09-10 10:20:11 +0200

    menu

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menu = document.getElementById('mainMenu');
+const headerIcon = document.getElementById('headerIconNav')
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    menu.classList.add("main-menu-open");
+    headerIcon.style.display = 'none';
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    menu.classList.remove("main-menu-open");
+    headerIcon.style.display = 'block';
     menuOpen = false;
   }
 }); 
+
